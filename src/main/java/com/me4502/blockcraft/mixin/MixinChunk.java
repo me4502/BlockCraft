f66904e1e770c1be32c84bad54cc6174b454274d
Matthew Miller <mnmiller1@me.com>
2017-08-12 09:54:01 +1000

    Actually get the mod running

--- src/main/java/com/me4502/blockcraft/mixin/MixinChunk.java
+++ src/main/java/com/me4502/blockcraft/mixin/MixinChunk.java
@@ -6,12 +6,13 @@ import net.minecraft.world.chunk.Chunk;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Chunk.class)
 public abstract class MixinChunk {
 
     @Inject(method = "setBlockState", at = @At("HEAD"))
-    public void onBlockStateChange(BlockPos pos, IBlockState state) {
-
+    public void onBlockStateChange(BlockPos pos, IBlockState state, CallbackInfoReturnable cir) {
+        System.out.println("Ayyo at " + pos.toString());
     }
 }
