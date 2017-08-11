f71fdde63dae279498788bc2534088862d7202b9
Matthew Miller <mnmiller1@me.com>
2017-08-12 08:41:10 +1000

    Added some more blockchain code

--- src/main/java/com/me4502/blockcraft/blockchain/BlockData.java
+++ src/main/java/com/me4502/blockcraft/blockchain/BlockData.java
@@ -1,5 +1,16 @@
 package com.me4502.blockcraft.blockchain;
 
-public class BlockData {
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.util.math.BlockPos;
 
+public class BlockData {
+    private String worldName;
+    private BlockPos blockPos;
+    private IBlockState blockState;
+
+    public BlockData(String worldName, BlockPos blockPos, IBlockState blockState) {
+        this.worldName = worldName;
+        this.blockPos = blockPos;
+        this.blockState = blockState;
+    }
 }
