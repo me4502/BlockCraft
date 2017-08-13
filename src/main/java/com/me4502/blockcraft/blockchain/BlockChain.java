0bc11b380d60c63a76f26a901988f08ed2b4c1c1
Madeline Miller <mnmiller1@me.com>
2017-08-13 16:00:40 +1000

    Added loading/saving

--- src/main/java/com/me4502/blockcraft/blockchain/BlockChain.java
+++ src/main/java/com/me4502/blockcraft/blockchain/BlockChain.java
@@ -1,24 +1,101 @@
 package com.me4502.blockcraft.blockchain;
 
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Lists;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonSerializer;
+import com.google.gson.reflect.TypeToken;
+import net.minecraft.block.Block;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.init.Blocks;
 import net.minecraft.util.math.BlockPos;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.List;
 import java.util.Objects;
 
 public class BlockChain {
 
+    private static Gson gson = new GsonBuilder().registerTypeAdapter(BlockElement.class, (JsonSerializer<BlockElement>) (src, typeOfSrc, context) -> {
+        JsonObject object = new JsonObject();
+
+        object.addProperty("index", src.getIndex());
+        object.addProperty("previousHash", src.getPreviousHash());
+        object.addProperty("hash", src.getHash());
+        object.addProperty("timestamp", src.getTimestamp());
+
+        JsonObject dataObject = new JsonObject();
+        JsonObject blockPosObject = new JsonObject();
+        blockPosObject.addProperty("x", src.getData().blockPos.getX());
+        blockPosObject.addProperty("y", src.getData().blockPos.getY());
+        blockPosObject.addProperty("z", src.getData().blockPos.getZ());
+
+        dataObject.add("blockPos", blockPosObject);
+
+        String name = String.valueOf(Block.REGISTRY.getNameForObject(src.getData().blockState.getBlock()));
+        if (src.getData().blockState.getBlock().getMetaFromState(src.getData().blockState) > 0) {
+            name += ";" + src.getData().blockState.getBlock().getMetaFromState(src.getData().blockState);
+        }
+
+        dataObject.addProperty("blockState", name);
+        object.add("data", dataObject);
+
+        return object;
+    }).registerTypeAdapter(BlockElement.class, (JsonDeserializer<BlockElement>) (json, typeOfT, context) -> {
+        int index = json.getAsJsonObject().get("index").getAsInt();
+        String previousHash = json.getAsJsonObject().get("previousHash").getAsString();
+        String hash = json.getAsJsonObject().get("hash").getAsString();
+        long timestamp = json.getAsJsonObject().get("timestamp").getAsLong();
+
+        JsonObject dataObject = json.getAsJsonObject().getAsJsonObject("data");
+
+        JsonObject blockPosObject = dataObject.getAsJsonObject("blockPos");
+        BlockPos blockPos = new BlockPos(blockPosObject.get("x").getAsInt(), blockPosObject.get("y").getAsInt(), blockPosObject.get("z").getAsInt());
+        String serialisedBlockState = dataObject.get("blockState").getAsString();
+        IBlockState blockState;
+        if (serialisedBlockState.contains(";")) {
+            String[] bits = serialisedBlockState.split(";");
+            Block block = Block.getBlockFromName(bits[0]);
+            blockState = block.getStateFromMeta(Integer.parseInt(bits[1]));
+        } else {
+            Block block = Block.getBlockFromName(serialisedBlockState);
+            blockState = block.getDefaultState();
+        }
+        return new BlockElement(index, previousHash, timestamp, new BlockData(blockPos, blockState), hash);
+    }).create();
+
+    private String worldName;
+
     private List<BlockElement> chainList = Lists.newArrayList(getInitialBlock());
+
+    public BlockChain(String worldName) {
+        this.worldName = worldName;
+    }
 
     public BlockElement getInitialBlock() {
         return new BlockElement(
                 0,
                 "",
                 System.nanoTime(),
-                new BlockData("", new BlockPos(0, 0, 0), Blocks.AIR.getDefaultState()),
+                new BlockData(new BlockPos(0, 0, 0), Blocks.AIR.getDefaultState()),
                 "abcdefghijklmnopqrstuvwxyz"
         );
+    }
+
+    public boolean addBlock(BlockElement element) {
+        return isNodeValid(element, getLatestBlock()) && chainList.add(element);
+    }
+
+    public ImmutableList<BlockElement> getChain() {
+        return ImmutableList.copyOf(this.chainList);
     }
 
     public BlockElement getLatestBlock() {
@@ -69,6 +146,32 @@ public class BlockChain {
             // TODO Sync with world.
         } else {
             System.out.println("Received BlockChain is invalid!");
+        }
+    }
+
+    public void load() {
+        System.out.println("Loading the chainzzz");
+        File file = new File(worldName + ".chainz");
+        if (file.exists()) {
+            try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+                chainList = gson.fromJson(reader, new TypeToken<List<BlockElement>>() {}.getType());
+                if (chainList == null) {
+                    // Heck, something got messed up
+                    chainList = Lists.newArrayList(getInitialBlock());
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void save() {
+        System.out.println("Saving the chainzzz");
+        File file = new File(worldName + ".chainz");
+        try(PrintWriter writer = new PrintWriter(new FileWriter(file))) {
+            writer.append(gson.toJson(chainList));
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
--- src/main/java/com/me4502/blockcraft/mixin/MixinChunk.java
+++ src/main/java/com/me4502/blockcraft/mixin/MixinChunk.java
@@ -1,9 +1,15 @@
 package com.me4502.blockcraft.mixin;
 
+import com.me4502.blockcraft.BlockCraft;
+import com.me4502.blockcraft.blockchain.BlockChain;
+import com.me4502.blockcraft.blockchain.BlockData;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 import net.minecraft.world.chunk.Chunk;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -11,8 +17,13 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(Chunk.class)
 public abstract class MixinChunk {
 
+    @Shadow
+    @Final
+    private World world;
+
     @Inject(method = "setBlockState", at = @At("HEAD"))
     public void onBlockStateChange(BlockPos pos, IBlockState state, CallbackInfoReturnable cir) {
-        System.out.println("Ayyo at " + pos.toString());
+        BlockChain blockChain = BlockCraft.instance.blockChains.get(world.getWorldInfo().getWorldName() + "," + world.provider.getDimension());
+        blockChain.addBlock(blockChain.generateNextBlock(new BlockData(pos, state)));
     }
 }
